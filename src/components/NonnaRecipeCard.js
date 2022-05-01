84af30cdd1ec4d9c39d713e6b8a3f0b1394465be
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-05-01 20:33:06 -0300

    refactor table in NonnaTopRecipes

--- src/components/NonnaRecipeCard.js
+++ src/components/NonnaRecipeCard.js
@@ -5,23 +5,23 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
-export default function NonnaRecipeCard() {
+export default function NonnaRecipeCard(props) {
     return (
-        /* utilizar props */
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
                 component="img"
                 height="140"
-                image="https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
-                alt="Pollo Tandori"
+                width="140"
+                image={props.imageUrl}
+                alt={props.title}
             />
             <CardContent>
                 <Link to='/receta' style={{ textDecoration: 'none' }}>
                     <Typography gutterBottom variant="h5" component="div">
-                        Pollo Tandori
+                        {props.title}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo creo queusted entiende
+                        {props.description}
                     </Typography>
                 </Link>
             </CardContent>
--- src/components/NonnaTopRecipes.js
+++ src/components/NonnaTopRecipes.js
@@ -1,24 +1,64 @@
 import * as React from 'react';
-import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Table, TableBody, TableCell, TableContainer, TableRow } from '@material-ui/core';
 import NonnaRecipeCard from './NonnaRecipeCard';
 
 export default function NonnaTopRecipes() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Grid container spacing={3}>
-                <Grid item xs={3}>
-                    <NonnaRecipeCard />
-                </Grid>
-                <Grid item xs={3}>
-                    <NonnaRecipeCard />
-                </Grid>
-                <Grid item xs={3}>
-                    <NonnaRecipeCard />
-                </Grid>
-                <Grid item xs={3}>
-                    <NonnaRecipeCard />
-                </Grid>
-            </Grid>
-        </div>
+        <TableContainer>
+            <Table sx={{ minWidth: 700 }} aria-label="Recetas destacadas">
+                <TableBody>
+
+                    <TableRow>
+                    {rows.map((row) => (
+                        <TableCell className={classes.cell} scope="row">
+                            <NonnaRecipeCard imageUrl={row.imageUrl} title={row.title} description={row.description} />
+                        </TableCell>
+                    ))}
+                    </TableRow>
+
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
+
+const useStyles = makeStyles(() => ({
+    cell: {
+      minWidth: '300px',
+      maxWidth: '300px'
+    },
+}));
+
+function createData(title, description, imageUrl) {
+    return {
+        title,
+        description,
+        imageUrl
+    };
+}
+
+const rows = [
+    createData('Pollo teriyaki',
+        "Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo",
+        "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    ),
+    createData('Tonkatsu',
+        "Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo",
+        "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    ),
+    createData('Milangas de oro',
+        "Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo",
+        "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    ),
+    createData('Cheese burger',
+        "Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo",
+        "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    ),
+    createData('Fideos con salsa',
+        "Receta de pollo que pollo y la pollo el pollo etc. Casi un lorem ipsum pero version pollo por pollo",
+        "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    ),
+];
