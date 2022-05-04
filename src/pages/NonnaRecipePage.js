3e54a72b5cd2fb91ea4972d2161478b31e9ba82b
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-05-04 20:54:17 -0300

    added step component (#6)
    * added step component
    
    * fix warnings

--- src/pages/NonnaRecipePage.js
+++ src/pages/NonnaRecipePage.js
@@ -4,6 +4,7 @@ import NonnaSectionSubtitle from "../components/NonnaSectionSubtitle";
 import NonnaReview from "../components/NonnaReview";
 import NonnaCreateReview from "../components/NonnaCreateReview";
 import NonnaRecipeResume from "../components/NonnaRecipeResume";
+import NonnaRecipeStep from "../components/NonnaRecipeStep";
 
 export default function NonnaRecipePage() {
   return (
@@ -17,6 +18,17 @@ export default function NonnaRecipePage() {
         vegan={true}
         celiac={true} />
       <NonnaRecipeIngredients />
+      <NonnaSectionSubtitle sectionTitle="Cómo preparar Pollo teriyaki" />
+      <NonnaRecipeStep
+        stepTitle="Preparar el pollo"
+        stepDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam."
+        stepImage="https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+      />
+      <NonnaRecipeStep
+        stepTitle="Cocinar en cacerola"
+        stepDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam."
+        stepImage="https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+      />
       <NonnaSectionSubtitle sectionTitle="Reseñas" />
       <NonnaReview
         reviewUser="Vito Corleone"
