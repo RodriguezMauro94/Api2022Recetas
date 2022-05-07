fac79aef653ffa862762e6b3050fa00b16ec1fde
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-05-07 13:05:54 -0300

    fix responsivness in resume

--- src/components/NonnaRecipeResume.js
+++ src/components/NonnaRecipeResume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Box } from '@material-ui/core';
 import { Stack } from '@mui/material';
