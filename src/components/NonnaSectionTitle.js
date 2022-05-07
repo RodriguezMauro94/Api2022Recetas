284490a1c166e80d1e48e807a0e98e10673a9131
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-05-07 12:12:02 -0300

    fix responsivness in sectionTitle

--- src/components/NonnaSectionTitle.js
+++ src/components/NonnaSectionTitle.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import { useMediaQuery } from '@material-ui/core';
 
 export default function NonnaSectionTitle(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={classes.sectionRow}>
+      {isMobile ?
+        <></>
+        :
         <div className={classes.sectionDivider}></div>
-        <Typography variant="h5">
-            {props.sectionTitle}
-        </Typography>
-        <div className={classes.sectionDivider}></div>
+      }
+      <Typography variant="h5">
+        {props.sectionTitle}
+      </Typography>
+      <div className={classes.sectionDivider}></div>
     </div>
   );
 }
@@ -22,8 +29,14 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     marginTop: "24px",
     marginBottom: "24px",
-    paddingLeft: "100px",
-    paddingRight: "100px",
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: "100px",
+      paddingRight: "100px",
+    },
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: "20px",
+      paddingRight: "20px",
+    },
     gap: "5px"
   },
   sectionDivider: {
@@ -31,6 +44,6 @@ const useStyles = makeStyles((theme) => ({
     width: "25px",
     height: "2px",
     flexGrow: 1,
-    
+
   }
 }));
