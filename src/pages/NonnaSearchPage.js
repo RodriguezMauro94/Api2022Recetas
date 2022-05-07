f1e5c954d99c923f61a9b479dbd0816ab964c704
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-05-07 03:18:10 -0300

    implement basic search

--- src/pages/NonnaSearchPage.js
+++ src/pages/NonnaSearchPage.js
@@ -4,23 +4,43 @@ import NonnaRecipeResume from "../components/NonnaRecipeResume";
 import recipes from '../mocks/recipes.json';
 import difficulties from '../mocks/difficulties.json';
 import NonnaFilterBar from "../components/NonnaFilterBar";
+import categories from "../mocks/categories.json"
+import { useParams } from "react-router-dom";
 
 export default function NonnaSearchPage() {
+    let params = useParams();
+    let searchRecipes = filterRecipes(params.search);
+
     return (
         <>
             <NonnaFilterBar />
-            {recipes.map((recipe) => (
-                 <NonnaLink to={`/receta/${recipe.id}`}>
-                 <NonnaRecipeResume
-                     image={recipe.urlImage}
-                     recipeTitle={recipe.name}
-                     recipeDescription={recipe.description}
-                     rating={recipe.ratings}
-                     difficulty={difficulties.find(difficulty => difficulty.key === recipe.difficulty).value}
-                     vegan={recipe.vegan}
-                     celiac={recipe.celiac} />
-             </NonnaLink>
+            {searchRecipes.map((recipe) => (
+                <NonnaLink to={`/receta/${recipe.id}`}>
+                    <NonnaRecipeResume
+                        image={recipe.urlImage}
+                        recipeTitle={recipe.name}
+                        recipeDescription={recipe.description}
+                        rating={recipe.ratings}
+                        difficulty={difficulties.find(difficulty => difficulty.key === recipe.difficulty).value}
+                        vegan={recipe.vegan}
+                        celiac={recipe.celiac} />
+                </NonnaLink>
             ))}
         </>
     );
+}
+
+function filterRecipes(search) {
+    if(search === undefined) {
+        return recipes;
+    } else {
+        return recipes.filter(
+            recipe => recipe.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+    }
+    /*
+    ||
+            recipe.category === 
+                categories.find(category => category.description.includes(search)).id
+    */
 }
