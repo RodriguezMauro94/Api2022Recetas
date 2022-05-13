5b6272e1acf4d6728a8d7bfca3d228b7457f8a00
Franco Benito <francobenito@protonmail.com>
2022-05-13 16:30:39 -0300

    Add modify recipe responsive

---
+++ src/pages/NonnaRecipeModifyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NonnaRecipeModify from "../components/NonnaRecipeModify";
+
+export default function NonnaRecipeModifyPage() {
+  return (
+    <>
+        <NonnaRecipeModify />
+    </>
+  );
+}
