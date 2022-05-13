d2f19e166ae39761a4e21c1b84fdb8a2dcfec3e4
Franco Benito <francobenito@protonmail.com>
2022-05-13 20:00:54 -0300

    Update NonnaAboutUs.js

--- src/components/NonnaAboutUs.js
+++ src/components/NonnaAboutUs.js
@@ -1,37 +1,85 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { Paper } from '@mui/material';
-import recipes from '../mocks/recipes.json'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { Container } from "@mui/material";
 
 export default function NonnaAboutUs() {
+    const theme = useTheme();
     const classes = useStyles();
-
     return (
-        <Paper elevation={0} sx={{ p: 2, margin: 1, maxWidth: 'auto', flexGrow: 1, marginTop: 2 }}>
-            <div className={classes.root}>
-                <Typography variant="h4" >
-                    Sobre La Nonna
-                </Typography>
+        <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: 'url(https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80)',
+      }}
+    >
 
-                <Typography variant="body1" >
-                    Brindamos un espacio para que puedas compartir las recetas de tu Nonna. También podrás aprender las recetas de las Nonnas de otros usuarios y calificarlas!
-                </Typography>
-            </div>
-        </Paper>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              Quienes somos?
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Sobre La Nonna
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm" className={classes.container}>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Nosotros
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            Brindamos un espacio para que puedas compartir las recetas de tu Nonna. 
+            También podrás aprender las recetas de las Nonnas de otros usuarios y calificarlas!
+            </Typography>
+          </Container>
+        </Box>
+    </Paper>
     );
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        marginTop: "25px",
-        [theme.breakpoints.up('md')]: {
-            marginLeft: "100px",
-            marginRight: "100px",
-        },
-        [theme.breakpoints.down('md')]: {
-            marginLeft: "20px",
-            marginRight: "20px",
-        },
-    },
+    container:{
+        backgroundColor: "rgba(0, 0, 0, 0)"
+    }
 }));
